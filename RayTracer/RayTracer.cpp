8e8df22c4b2aa8194284ff601b1b1d9865adea5e
radixx-skip <schipper.joseph@gmail.com>
2017-08-18 23:08:20 -0400

    Added Cimg library and test code.

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+#include "CImg.h"
 
 int main()
 {
+	cimg_library::CImg<unsigned char> test(256, 256, 1, 3, 0);
+	cimg_library::CImgDisplay disp;
+	for (int i = 0; i < 255; i++)
+	{
+		for (int j = 0; j < 255; j++)
+		{
+			test(i, j, 1) = i;
+			test(i, j, 2) = j;
+			disp = test;
+		}
+	}
+	disp.close();
+	test.display();
     return 0;
 }
 
